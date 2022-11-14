30cadd56591cbc3046ec4c37a09af0316f5ed2a7
adao <aldo.santana@gft.com>
2022-11-14 13:51:07 -0300

    Add transactional

--- src/main/java/com/example/dto/service/UserService.java
+++ src/main/java/com/example/dto/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.dto.dto.UserDto;
 import com.example.dto.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -12,7 +13,7 @@ public class UserService {
     @Autowired
     private UserRepositories repositories;
 
-
+    @Transactional(readOnly = true)
     public UserDto findById(Long id) {
         User entity = repositories.findById(id).get();
         UserDto dto = new UserDto(entity);
